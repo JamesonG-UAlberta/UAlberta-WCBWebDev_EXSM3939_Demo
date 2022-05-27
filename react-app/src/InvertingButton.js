347974cfd02c16e8cb98767b32af4ae30c968a4c
James Grieve <jgrieve@ualberta.ca>
2022-05-27 11:16:33 -0600

    Exercise 01 constructor;

--- react-app/src/InvertingButton.js
+++ react-app/src/InvertingButton.js
@@ -2,7 +2,13 @@ import './App.css';
 import React from 'react';
 
 class InvertingButton extends React.Component {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            changedColour: false
+        };
+    }
 
     render() {
         return (<button>Click Me!</button>);

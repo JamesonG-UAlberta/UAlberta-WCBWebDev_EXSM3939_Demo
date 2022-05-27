7df42ba919094a6d43257badfe326a31b3500cd2
James Grieve <jgrieve@ualberta.ca>
2022-05-27 10:39:06 -0600

    Exercise 01 skeleton;

---
+++ react-app/src/InvertingButton.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+
+class InvertingButton extends React.Component {
+
+
+    render() {
+        return (<button>Click Me!</button>);
+    }
+}
+
+export default InvertingButton;
